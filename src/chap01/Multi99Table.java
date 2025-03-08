863d6895330054dfe5803fb10f4faa2ab18e6795
Poby <smg981024@gmail.com>
2025-03-09 02:26:45 +0900

    feat: 구구단 연습문제

--- src/chap01/Multi99Table.java
+++ src/chap01/Multi99Table.java
@@ -4,7 +4,14 @@ public class Multi99Table {
     public static void main(String[] args) {
         System.out.println("-----구구단 곱셈표------");
 
+        System.out.print("   |");
+        for (int a = 1; a <= 9; a++) {
+            System.out.printf("%3d", a);
+        }
+        System.out.println("\n---+------------------------------");
+
         for (int i = 1; i <= 9; i++) {
+            System.out.printf("%2d |", i);
             for (int j = 1; j <= 9; j++)
                 System.out.printf("%3d", i * j);
             System.out.println();
