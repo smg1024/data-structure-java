1c5a507c3296d819bf064f4df2c77bde7c3524df
Poby <smg981024@gmail.com>
2025-03-12 16:39:38 +0900

    feat: 클래스에서 배열 구현하기

---
+++ src/chap02/PhysicalExamination.java
@@ -0,0 +1,70 @@
+package chap02;
+
+class PhysicalExamination {
+
+    static final int VMAX = 21;
+
+    static class PhysicalData {
+        String name;
+        int height;
+        double vision;
+
+        PhysicalData(String name, int height, double vision) {
+            this.name = name;
+            this.height = height;
+            this.vision = vision;
+        }
+    }
+
+    static double averageHeight(PhysicalData[] dataArray) {
+        double sum = 0;
+
+        for (PhysicalData data : dataArray) {
+            sum += data.height;
+        }
+
+        return sum / dataArray.length;
+    }
+
+    static void distributeVision(PhysicalData[] dataArray, int[] dist) {
+        int i = 0;
+        dist[i] = 0;
+
+        for (i = 0; i < dataArray.length; i++) {
+            if (dataArray[i].vision >= 0.0 && dataArray[i].vision <= VMAX / 10.0) {
+                dist[(int) (dataArray[i].vision * 10)]++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PhysicalData[] studentList = {
+                new PhysicalData("강민하", 162, 0.3),
+                new PhysicalData("김찬우", 173, 0.7),
+                new PhysicalData("박준서", 175, 2.0),
+                new PhysicalData("유서범", 171, 1.5),
+                new PhysicalData("이수연", 168, 0.4),
+                new PhysicalData("장경오", 174, 1.2),
+                new PhysicalData("황지안", 169, 0.8)
+        };
+        int[] visionDistribution = new int[VMAX];
+
+        System.out.println("--신체검사 리스트--");
+        System.out.println("이름     키     시력");
+        System.out.println("-------------------");
+
+        for (int i = 0; i < studentList.length; i++) {
+            System.out.printf("%-8s%3d%5.1f\n",
+                    studentList[i].name, studentList[i].height, studentList[i].vision);
+        }
+
+        System.out.printf("\n평균 키: %5.1fcm\n", averageHeight(studentList));
+
+        distributeVision(studentList, visionDistribution);
+
+        System.out.println("\n시력 분포");
+        for (int i = 0; i < VMAX; i++) {
+            System.out.printf("%3.1f~: %2d명\n", i / 10.0, visionDistribution[i]);
+        }
+    }
+}
