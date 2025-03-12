072a5a9cb3ec8aef0e3a7482bc66b1d4907491f9
Poby <smg981024@gmail.com>
2025-03-12 16:12:01 +0900

    feat: 소수 찾기 개선 2

--- src/chap02/PrimeNumber.java
+++ src/chap02/PrimeNumber.java
@@ -3,21 +3,28 @@ package chap02;
 class PrimeNumber {
     public static void main(String[] args) {
         int counter = 0;
-        int indexPointer = 0;
+        int ptr = 0;
         int[] prime = new int[500];
 
-        prime[indexPointer++] = 2;
+        prime[ptr++] = 2;
+        prime[ptr++] = 3;
 
-        for (int n = 3; n <= 1000; n += 2) {
-            int i;
-            for (i = 1; i < indexPointer; i++) {
-                counter++;
-                if (n % prime[i] == 0) break;
+        for (int n = 5; n <= 1000; n += 2) {
+            boolean isPrime = true;
+            for (int i = 1; prime[i] * prime[i] <= n; i++) {
+                counter += 2;
+                if (n % prime[i] == 0) {
+                    isPrime = false;
+                    break;
+                }
             }
-            if (indexPointer == i) prime[indexPointer++] = n;
+            if (isPrime) {
+                prime[ptr++] = n;
+                counter++;
+            }
         }
 
-        for (int i = 0; i < indexPointer; i++) {
+        for (int i = 0; i < ptr; i++) {
             System.out.println(prime[i]);
         }
 
