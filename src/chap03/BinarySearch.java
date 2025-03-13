37e7da6d2ae6f087b5aedba6c4516535f61340e9
Poby <smg981024@gmail.com>
2025-03-13 15:10:49 +0900

    feat: 중복값을 고려한 이진 탐색

--- src/chap03/BinarySearch.java
+++ src/chap03/BinarySearch.java
@@ -18,6 +18,22 @@ class BinarySearch {
         return -1;
     }
 
+    static int binarySearchX(int[] a, int n, int key) {
+        int left = 0;
+        int right = n - 1;
+
+        do {
+            int center = (left + right) / 2;
+            if (a[center] == key) {
+                while (a[center - 1] == key) center -= 1;
+                return center;
+            } else if (a[center] < key) left = center + 1;
+            else right = center - 1;
+
+        } while (left <= right);
+        return -1;
+    }
+
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
 
@@ -37,7 +53,7 @@ class BinarySearch {
         System.out.print("검색할 값: ");
         int key = stdIn.nextInt();
 
-        int idx = binarySearch(x, num, key);
+        int idx = binarySearchX(x, num, key);
 
         if (idx == -1) System.out.println("검색할 값이 존재하지 않습니다.");
         else System.out.printf("그 값은 x[%d]에 있습니다.\n", idx);
