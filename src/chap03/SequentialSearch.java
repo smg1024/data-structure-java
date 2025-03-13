b82025ade1571676d6c6afca28fd76e139c0c813
Poby <smg981024@gmail.com>
2025-03-13 14:23:20 +0900

    feat: 선형 검색 보초법

--- src/chap03/SequentialSearch.java
+++ src/chap03/SequentialSearch.java
@@ -19,12 +19,24 @@ class SequentialSearch {
 //        }
     }
 
+    static int seqSearchSentinel(int[] a, int n, int key) {
+        a[n] = key;
+
+        int i = 0;
+        while (true) {
+            if (a[i] == key) break;
+            i++;
+        }
+
+        return i == n ? -1 : i;
+    }
+
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
 
         System.out.print("요솟수: ");
         int num = stdIn.nextInt();
-        int[] x = new int[num];
+        int[] x = new int[num + 1];
 
         for (int i = 0; i < num; i++) {
             System.out.printf("x[%d]: ", i);
@@ -35,9 +47,9 @@ class SequentialSearch {
         System.out.print("검색할 값: ");
         int key = stdIn.nextInt();
 
-        int idx = seqSearch(x, num, key);
+        int idx = seqSearchSentinel(x, num, key);
 
         if (idx == -1) System.out.println("검색할 값이 존재하지 않습니다.");
-        else System.out.printf("그 값은 x[%d]에 있습니다.", idx);
+        else System.out.printf("그 값은 x[%d]에 있습니다.\n", idx);
     }
 }
