32bbbc1d2ddbb6dfc58eec455aa0cf08bc90cf4f
Poby <smg981024@gmail.com>
2025-03-13 15:28:13 +0900

    feat: 클래스 메서드와 인스턴스 메서드 예제

---
+++ src/chap03/IdTester.java
@@ -0,0 +1,30 @@
+package chap03;
+
+class Id {
+    private static int counter = 0;
+    private int id;
+
+    public Id() {
+        id = ++counter;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
+public class IdTester {
+    public static void main(String[] args) {
+        Id a = new Id();
+        Id b = new Id();
+
+        System.out.println("a.getId() = " + a.getId());
+        System.out.println("b.getId() = " + b.getId());
+
+        System.out.println("Id.getCounter() = " + Id.getCounter());
+    }
+}
