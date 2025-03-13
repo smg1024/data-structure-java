bb08b39df6979ff882321712565f8efed9fa1027
Poby <smg981024@gmail.com>
2025-03-13 14:06:26 +0900

    feat: 선형 검색 for문

--- src/chap03/SequentialSearch.java
+++ src/chap03/SequentialSearch.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 
 class SequentialSearch {
     static int seqSearch(int[] a, int n, int key) {
-        int i = 0;
-
-        while (true) {
-            if (i == n) return -1;
+        for (int i = 0; i < n; i++) {
             if (a[i] == key) return i;
-            i++;
         }
+        return -1;
+
+//        int i = 0;
+//
+//        while (true) {
+//            if (i == n) return -1;
+//            if (a[i] == key) return i;
+//            i++;
+//        }
     }
 
     public static void main(String[] args) {
