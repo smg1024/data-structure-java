ba9969e8055419a5f4c773102fe17b0a721e522d
Poby <smg981024@gmail.com>
2025-03-19 15:15:44 +0900

    feat(recursive): gcd array

--- src/chap05/EuclidGCD.java
+++ src/chap05/EuclidGCD.java
@@ -8,6 +8,16 @@ class EuclidGCD {
         else return gcd(y, x % y);
     }
 
+    static int gcdArray(int[] a) {
+        int result = gcd(a[0], a[1]);
+
+        for (int i = 2; i < a.length; i++) {
+            result = gcd(result, a[i]);
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -19,5 +29,8 @@ class EuclidGCD {
         int y = scanner.nextInt();
 
         System.out.println("gcd(x, y) = " + gcd(x, y));
+
+        int[] a = {6, 15, 96, 1254};
+        System.out.println("gcdArray(a) = " + gcdArray(a));
     }
 }
